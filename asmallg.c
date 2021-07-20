flamewing/asl-releases

commit 68d40439679ac9af1f24a783188cd4df900b6d0d

Making -q work like -x
Psssing -q once disables all output but "message" directives.
Passing -q twice (or -qq) suprpesses "message" as well.
This makes -qq work like "official" AS, and -q work like it used to.

asmallg.c
@@ -804,7 +804,7 @@ static void CodeMESSAGE(Word Index)
     if (!OK) WrError(ErrNum_InvString);
     else
     {
-      //if (!QuietMode)
+      if (QuietMode != 2)
         WrConsoleLine(mess, True);
       if (strcmp(LstName, "/dev/null"))
         WrLstLine(mess);

as.c
@@ -3732,8 +3732,11 @@ static CMDResult CMD_SplitByte(Boolean Negate, const char *Arg)
 static CMDResult CMD_QuietMode(Boolean Negate, const char *Arg)
 {
   UNUSED(Arg);
+  if ((Negate) && (QuietMode > 0))
+    QuietMode--;
+  else if ((!Negate) && (QuietMode < 2))
+    QuietMode++;
 
-  QuietMode = !Negate;
   return CMDOK;
 }
 

asmdef.c
@@ -96,7 +96,7 @@ Boolean NumericErrors;                   /* Fehlermeldungen mit Nummer */
 Boolean CodeOutput;		         /* Code erzeugen */
 Boolean MacProOutput;                    /* Makroprozessorausgabe schreiben */
 Boolean MacroOutput;                     /* gelesene Makros schreiben */
-Boolean QuietMode;                       /* keine Meldungen */
+ShortInt QuietMode;                      /* keine Meldungen */
 Boolean HardRanges;                      /* Bereichsfehler echte Fehler ? */
 const char *DivideChars;                 /* Trennzeichen fuer Parameter. Inhalt Read Only! */
 Boolean HasAttrs;                        /* Opcode hat Attribut */

asmdef.h
@@ -299,7 +299,7 @@ extern Boolean NumericErrors;
 extern Boolean CodeOutput;
 extern Boolean MacProOutput;
 extern Boolean MacroOutput;
-extern Boolean QuietMode;
+extern ShortInt QuietMode;
 extern Boolean HardRanges;
 extern const char *DivideChars;
 extern Boolean HasAttrs;

commit 5ff5d91dd737fef66c942c9e2ff9db458b5bf6c6

Fixing message with -q (was broken by 68d40439)

asmallg.c
@@ -819,7 +819,7 @@ static void CodeMESSAGE(Word Index)
     if (!OK) WrError(ErrNum_InvString);
     else
     {
-      if (QuietMode != 2)
+      if (QuietMode < 2)
         WrConsoleLine(mess, True);
       if (strcmp(LstName, "/dev/null"))
         WrLstLine(mess);