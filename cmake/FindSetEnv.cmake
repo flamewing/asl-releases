dfffaf372a9ff4132f8ff510a9dc7d6eae19acc8
flamewing <flamewing.sonic@gmail.com>
2024-04-19 09:48:46 +0200

    Applying formatter to cmake stuff

--- cmake/FindSetEnv.cmake
+++ cmake/FindSetEnv.cmake
@@ -2,6 +2,7 @@
 
 find_program(WINSDK_SETENV NAMES SetEnv.cmd
     PATHS "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows;CurrentInstallFolder]/bin")
-if (WINSDK_SETENV AND PRINT_PATH)
+
+if(WINSDK_SETENV AND PRINT_PATH)
     execute_process(COMMAND ${CMAKE_COMMAND} -E echo "${WINSDK_SETENV}")
-endif ()
+endif()
